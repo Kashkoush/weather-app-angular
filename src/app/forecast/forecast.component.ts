8901bd9d9261cd034b46980130a377659b5b0d24
Ahmed Kashkoush <kashkoush@e-phunk.com>
2020-02-01 20:20:34 +0200

    current weather updated

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { WeatherServiceService } from '../weather-service.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-forecast',
@@ -8,14 +10,44 @@ import { NgForm } from '@angular/forms';
 })
 export class ForecastComponent implements OnInit {
   @ViewChild('f', { static: false }) forecastForm: NgForm;
+  forecastNow: any;
+  selectedCityName: string;
+  requestTime: any;
+  timeInterval: any;
 
-  constructor() { }
+  constructor(private weatherService: WeatherServiceService) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.forecastForm.form.setValue({ cityName: 'cairo' });
+      this.onSubmit(this.forecastForm);
+    })
   }
 
   onSubmit(form: NgForm) {
     console.log(form.form.value);
+    this.forecastNow = false;
+    this.selectedCityName = form.form.value.cityName;
+    this.weatherService.getForecast(this.selectedCityName).subscribe((forecastData: any) => {
+      console.log(forecastData);
+      this.forecastNow = forecastData;
+      this.requestTime = moment().format();
+      this.getMoment();
+    }, error => {
+      console.log(error);
+      this.forecastNow = true;
+    })
+  }
+
+  convertKelvinToCelsius(kelvin) {
+    return kelvin - 273.15;
+  }
+
+  getMoment() {
+    setInterval(() => {
+      // console.log(moment(this.requestTime).fromNow());
+      this.timeInterval = moment(this.requestTime).fromNow();
+    }, 1000);
   }
 
 }
